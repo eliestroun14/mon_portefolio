a916e1cb0516d101ad9f9fbe9e5de08e0ca8be49
Elie Stroun <elie.Stroun@epitech.eu>
2024-02-13 10:02:02 +0100

    feat(page.jsx): add useTransform

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { useInView } from "framer-motion";
+import { useTransform } from "framer-motion";
 import { useScroll } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
